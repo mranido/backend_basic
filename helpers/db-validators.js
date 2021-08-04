9d4a8346f6746563a626ad66ebc8a92a20ba89f3
mranido <mranido.da@gmail.com>
2021-08-04 18:00:35 +0200

    Add Cloudinary

--- helpers/db-validators.js
+++ helpers/db-validators.js
@@ -37,7 +37,21 @@ const existeProducto = async (id) => {
   }
 };
 
+//Validar Colecciones Permitidas
+
+const coleccionesPermitidas = (coleccion = "", colecciones = []) => {
+  const incluida = colecciones.includes(coleccion);
+
+  if (!incluida) {
+    throw new Error(
+      `La coleccion ${coleccion} no es permitida, ${colecciones}`
+    );
+  }
+  return true;
+};
+
 module.exports = {
+  coleccionesPermitidas,
   esRoleValido,
   emailExiste,
   existeUsuarioPorId,
--- models/producto.js
+++ models/producto.js
@@ -29,6 +29,7 @@ const ProductoSchema = Schema({
     type: String,
   },
   disponible: { type: Boolean, default: true },
+  img: { type: String },
 });
 
 ProductoSchema.methods.toJSON = function () {
